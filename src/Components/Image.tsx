c262e08d39ac65c6406171cb6f1d0321ccea871d
Amaury <amaury.lopezdiaz@gmail.com>
2022-07-28 19:30:12 -0400

    ustarted working on image component

--- src/Components/Image.tsx
+++ src/Components/Image.tsx
@@ -1,61 +1,70 @@
+import axios from "axios"
+import { Request } from "express"
 import React from "react"
 import {useState} from 'react'
+
 
 
 const url = "https://server-for-social.onrender.com"
 const devUrl = "http://localhost:8001"
 
 const Image: React.FC = () => {
-    const [image, setImage] = useState()
+    const [file, setFile] = useState<string | Blob>()
+    const [fileArrayBuffer, setFileArrayBuffer] = useState<ArrayBuffer | string>()
+    const [fileName, setFileName] = useState<string>()
     
 
+   
+    // console.log(fileName)
+    // console.log(file)
     
     const handleChange = (e:React.ChangeEvent<HTMLInputElement>):void=>{
-        // const input = e.target.files![0]
-        // const reader = new FileReader();
-        // reader.onload!(function (event:ProgressEvent<FileReader>):void{
-        //     console.log("this never works : ", event);
-        //     console.log(event);
-        // });
-        // reader.readAsText(input)
-        // // setImage(e.target.value) 
-        // // e.target.files[0]
-        console.log(e)
-        
+        let fileInputted:Blob = e.target.files![0]
+        setFile(fileInputted)
+        setFileName(e.target.files?.[0].name) 
     }
 
+
+
     const handleImagePost = ():void =>{
-        const params = {
-           image:'image for now'
-        };
+        const formData = new FormData()
+
+        formData.append('file',file!)
+        formData.append('fileName', fileName!)
+
+        // interface RequestInit {
+        //     method: string,
+        //     body: {name:string}
+        // }
+      
         const options = {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(params)
+            body: JSON.stringify({name:formData})
+            
         };
-        
-        try {
-            fetch(`${devUrl}/postImageToS3`,options)
-                .then(res=>{console.log(res)}
-            )
-        }
-        catch {
-           
-        }
 
-        
-
+        try{
+            // fetch(`${devUrl}/postImageToS3`,options)
+            //         .then(res=>{return res.json()})
+            //         .then(data=>{console.log(data)}) 
+         
+        } 
+        catch(err){
+            console.log(err)
+        }
     }
 
 
     return (
     <>
     <div>COMING SOON</div>
-    <input type='file' id="chooseFileButton" onChange={(e)=>handleChange(e)}></input>
-    <button id="uploadImageButton"
-     onClick={()=>handleImagePost()}
-    >post</button>
-    <img src={image}></img>
+   
+        <input type='file' id="chooseFileButton" onChange={(e)=>handleChange(e)}></input>
+        <button id="uploadImageButton" onClick={()=>handleImagePost()}>post
+        </button>
+  
+    
     </>
     )
 }
